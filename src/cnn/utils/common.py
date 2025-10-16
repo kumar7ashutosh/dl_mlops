d2b7c9453721c62afef613b2a9835761c951f4b9
kumar7ashutosh <kumarashutoshbtech2023@gmail.com>
2025-10-16 21:05:49 +0530

    data ingestion

--- src/cnn/utils/common.py
+++ src/cnn/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from cnn import logger
+from src.cnn import logger
 import json
 import joblib
 from ensure import ensure_annotations
